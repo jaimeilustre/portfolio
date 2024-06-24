505cdc47c99c9838913314c432b407285d309c26
Jaime Ilustre <jaimeilustre@yahoo.com>
2024-06-24 11:08:24 +0200

    installed dependencies and added components

---
+++ src/components/BackgroundLines.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BackgroundLines(props){
+  return (
+    <div className="mi-bglines">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+
+export default BackgroundLines;
+
